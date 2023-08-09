4380f2f067648deb588adecccf71eca4527556c2
Arish82 <arish123.ahamad@gmail.com>
2023-08-09 19:45:50 +0530

    final deployment

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB = process.env.DATABASE;
+const DB="mongodb+srv://newArish:Arish%40123@cluster0.hggc0rs.mongodb.net/?retryWrites=true&w=majority"
 
 
 mongoose.connect(DB).then(() => {
